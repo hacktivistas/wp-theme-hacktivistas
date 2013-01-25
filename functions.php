fb1a6990a933d51c4a8356e3369682bc795da8dd
marimari <mari@colinaroja.org>
2013-01-26 00:59:55 +0100

    footer copyleft

--- functions.php
+++ functions.php
@@ -356,7 +356,7 @@ if ( ! function_exists( 'the_bootstrap_credits' ) ) :
  */
 function the_bootstrap_credits() {
 	printf(
-		'<span class="credits alignleft">' . __( '&copy; %1$s <a href="%2$s">%3$s</a>, all rights reserved.', 'the-bootstrap' ) . '</span>',
+		'<span class="credits alignleft">' . __( '(ɔ) %1$s <a href="%2$s">%3$s</a>, Copyleft.', 'the-bootstrap' ) . '</span>',
 		date( 'Y' ),
 		home_url( '/' ),
 		get_bloginfo( 'name' )
