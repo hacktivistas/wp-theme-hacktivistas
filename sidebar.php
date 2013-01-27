8b9fabd2d1305c3654b5d7bc57a62fe38ca75cf1
marimari <mari@colinaroja.org>
2013-01-27 01:52:45 +0100

    cabecera

--- sidebar.php
+++ sidebar.php
@@ -21,23 +21,24 @@ tha_sidebars_before(); ?>
 	
 	<?php tha_sidebar_top();
 	
-	if ( ! dynamic_sidebar( 'main' ) ) {
-		the_widget( 'WP_Widget_Archives', array(
-			'count'		=>	0,
-			'dropdown'	=>	0
-		), array(
-			'before_widget'	=>	'<aside id="archives" class="widget well widget_archives">',
-			'after_widget'	=>	'</aside>',
-			'before_title'	=>	'<h3 class="widget-title">',
-			'after_title'	=>	'</h3>',
-		) );
-		the_widget( 'WP_Widget_Meta', array(), array(
-			'before_widget'	=>	'<aside id="meta" class="widget well widget_meta">',
-			'after_widget'	=>	'</aside>',
-			'before_title'	=>	'<h3 class="widget-title">',
-			'after_title'	=>	'</h3>',
-		) );
-	} // end sidebar widget area
+	dynamic_sidebar( 'main' );
+	#if ( ! dynamic_sidebar( 'main' ) ) {
+	#	the_widget( 'WP_Widget_Archives', array(
+	#		'count'		=>	0,
+	#		'dropdown'	=>	0
+	#	), array(
+	#		'before_widget'	=>	'<aside id="archives" class="widget well widget_archives">',
+	#		'after_widget'	=>	'</aside>',
+	#		'before_title'	=>	'<h3 class="widget-title">',
+	#		'after_title'	=>	'</h3>',
+	#	) );
+	#	the_widget( 'WP_Widget_Meta', array(), array(
+	#		'before_widget'	=>	'<aside id="meta" class="widget well widget_meta">',
+	#		'after_widget'	=>	'</aside>',
+	#		'before_title'	=>	'<h3 class="widget-title">',
+	#		'after_title'	=>	'</h3>',
+	#	) );
+	#}  //end sidebar widget area 
 	
 	tha_sidebar_bottom(); ?>
 </section><!-- #secondary .widget-area -->
